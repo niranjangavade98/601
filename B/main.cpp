756dbabdefcec1cbfb19bd11e315ce44b9f06d50
niranjan98 <niranjangavade5@gmail.com>
2019-12-30 20:52:44 +0530

    Accepted!

--- B/main.cpp
+++ B/main.cpp
@@ -1,5 +1,35 @@
 #include<iostream>
-
+#include<map>
+#include<vector>
 int main(){
+    int cars,temp;
+    std::map<int,int> entry;
+    std::map<int,int>::iterator it;
+    std::vector<int> time;
+    std::cin>>cars;
+    for (int i = 1; i <= cars; i++)     //storing entry sequence with time in map
+    {
+        std::cin>>temp;
+        entry[temp]=i;
+        time.push_back(0);
+    }
+    for (int i = 1; i <= cars; i++)     //storing exit time according to exit sequence
+    {
+        std::cin>>temp;
+        it = entry.find(temp);
+        if(it!=entry.end()){
+            time[(it->second) -1]=i;
+        }
+    }
+    int max=time[0],count=0;
+    for(int i=1; i<cars; i++){
+        if(time[i]<max){
+            count++;
+        }
+        else{
+            max=time[i];
+        }
+    }
+    std::cout<<count<<std::endl;
     return 0;
 }
